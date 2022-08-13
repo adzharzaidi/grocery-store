f9485bc7c1ffbcc3629100cb39615f1f981edf83
rahzda <adzharzaidi@gmail.com>
2022-08-13 15:34:17 +0800

    first commit

---
+++ database/migrations/2022_08_13_071256_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('seller_id');
+            $table->string('name');
+            $table->double('price',8,2);
+            $table->text('description');
+            $table->string('category');
+            $table->text('image');
+            $table->integer('stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ resources/views/seller/index.blade.php
@@ -0,0 +1,2 @@
+@extends('seller.layouts.app')
+@section('content')
